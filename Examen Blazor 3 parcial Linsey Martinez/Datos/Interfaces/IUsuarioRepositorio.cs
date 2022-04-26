f7d1431c7252d8730ffdf03e3d299132a03cbb5c
lins0 <98849826+lins0@users.noreply.github.com>
2022-04-26 15:21:31 -0600

    cambios
    cambios

---
+++ Examen Blazor 3 parcial Linsey Martinez/Datos/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Modelos;
+
+namespace Datos.Interfaces;
+
+public interface IUsuarioRepositorio
+{
+    Task<bool> Nuevo(Usuario usuario);
+    Task<bool> Actualizar(Usuario usuario);
+    Task<bool> Eliminar(Usuario usuario);
+    Task<IEnumerable<Usuario>> GetLista();
+    Task<Usuario> GetPorCodigo(string codigo);
+    Task<bool> ValidaUsuario(Login login);
+}
